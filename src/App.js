92585e88610b7f95baa8ca2c79a273fff82c5d00
Nesgc <nestorgarciac11@gmail.com>
2025-03-18 20:38:04 -0600

    init

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/header/header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header /> 
+      <header className="App-header">
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
