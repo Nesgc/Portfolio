22d1313ba50e2fbb0826917b821007b7425bf11b
Nesgc <nestorofgarcia@gmail.com>
2025-12-08 23:22:05 -0600

    add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
-import { ThemeScript } from "./theme-script"; // 👈 adjust path if needed
+import { ThemeScript } from "./theme-script";
 import { Analytics } from "@vercel/analytics/react";
 
 export const metadata: Metadata = {
@@ -21,6 +22,18 @@ export default function RootLayout({
         <Analytics />
       </head>
       <body>{children}</body>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-ZY9R0VSX55"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-ZY9R0VSX55');
+        `}
+      </Script>
     </html>
   );
 }
